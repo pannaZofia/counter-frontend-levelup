9b4c6d7b3767a114489e93a1870e4070bb7e9fc6
Zofia Lecyk <lecyk.zofia@gmail.com>
2018-04-22 22:42:51 +0200

    Zadanie 2. Frontend LevelUp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Counter from './Counter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<Counter from="20" to="50" />, document.getElementById('root'));
